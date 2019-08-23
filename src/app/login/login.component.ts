3eff30486d093a6390a92c61d9c88723fff2bca9
IT-Agenturen <allan.dahl@itagenturen.com>
2019-08-23 21:50:26 +0200

    facebook Oath login added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var FB: any;
 
 @Component({
   selector: 'app-login',
@@ -9,7 +10,46 @@ export class LoginComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {  
+    (window as any).fbAsyncInit = function() {
+      FB.init({
+        appId      : '2495613174001179',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v3.1'
+      });
+      FB.AppEvents.logPageView();
+    };
+
+    (function(d, s, id){
+       var js, fjs = d.getElementsByTagName(s)[0];
+       if (d.getElementById(id)) {return;}
+       js = d.createElement(s); js.id = id;
+       js.src = "https://connect.facebook.net/en_US/sdk.js";
+       fjs.parentNode.insertBefore(js, fjs);
+     }(document, 'script', 'facebook-jssdk'));
   }
 
+  submitLogin(){
+    console.log("submit login to facebook");
+    // FB.login();
+    FB.login((response)=>
+        {
+          console.log('submitLogin',response);
+          if (response.authResponse)
+          {
+              
+           }
+           else
+           {
+           console.log('User login failed');
+         }
+      });
+
+  }
+ 
+
+  
+
 }
+
